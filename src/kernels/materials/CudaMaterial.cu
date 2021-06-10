fe3a3aa23813c2b029832fcdff73a0c7af8a7844
Ben Spencer <git@raytracing.co.uk>
2021-06-10 18:55:26 +0100

    Implement simple material shader.

---
+++ src/kernels/materials/CudaMaterial.cu
@@ -0,0 +1,44 @@
+﻿#include "CudaMaterial.cuh"
+
+namespace Cuda
+{
+    __device__ vec3 Device::SimpleMaterial::Evaluate(const HitCtx& hit) const
+    {
+        constexpr float kGridScale = 5.0f;
+
+        vec3 albedo = m_params.albedo;
+        vec2 absUv = abs(hit.uv - vec2(0.5f));
+        if (absUv.x < 0.52f && absUv.y < 0.52f && !(absUv.x < 0.5f && absUv.y < 0.5f)) 
+        { 
+            albedo *= 0.7; 
+        }
+        if (fract(absUv.x * kGridScale) < 0.02f || fract(absUv.y * kGridScale) < 0.02f ||
+            fract(absUv.x * 10.0f * kGridScale) < 0.1f || fract(absUv.y * 10.0 * kGridScale) < 0.1f)
+        {
+            albedo *= 0.7;
+        }
+
+        return albedo;
+    }
+    
+    __host__ Host::SimpleMaterial::SimpleMaterial() :
+        cu_deviceData(nullptr)
+    {
+        cu_deviceData = InstantiateOnDevice<Device::SimpleMaterial>();
+    }
+
+    __host__ void Host::SimpleMaterial::OnDestroyAsset()
+    {
+        DestroyOnDevice(&cu_deviceData);
+    }
+
+    __host__ void Host::SimpleMaterial::OnJson(const Json::Node& jsonNode)
+    {
+        Device::SimpleMaterial::Params params;
+
+        jsonNode.GetVector("albedo", params.albedo, true);
+
+        SyncParameters(cu_deviceData, params);
+    }
+}
+    
---
+++ src/kernels/materials/CudaMaterial.cuh
@@ -0,0 +1,73 @@
+﻿#pragma once
+
+#include "../CudaRay.cuh"
+#include "../CudaCtx.cuh"
+#include "generic/JsonUtils.h" 
+
+namespace Cuda
+{
+    namespace Host {  class Material;  }    
+
+    namespace Device
+    {
+        class Material : public Device::Asset, public AssetTags<Host::Material, Device::Material>
+        {
+        public:
+            Material() = default;
+
+            __device__ vec3 Evaluate(const HitCtx& hit) const;
+        };
+    }
+
+    namespace Host
+    {
+        class Material : public Host::Asset, public AssetTags<Host::Material, Device::Material>
+        {
+        public:
+            Material() = default;
+
+            __host__ virtual Device::Material* GetDeviceInstance() const = 0;
+        };
+    }
+
+    namespace Host { class SimpleMaterial; }
+
+    namespace Device
+    {
+        class SimpleMaterial : public Device::Material
+        {
+            friend Host::SimpleMaterial;
+        public:
+            struct Params
+            {
+                __host__ __device__ Params() : albedo(0.5f) {}
+                vec3 albedo;
+            }
+            m_params;
+
+        public:
+            SimpleMaterial() = default;
+            ~SimpleMaterial() = default;
+
+            __device__ vec3 Evaluate(const HitCtx& hit) const;
+            __device__ void OnSyncParameters(const Params& params) { m_params = params;  }
+        };
+    }
+
+    namespace Host
+    {
+        class SimpleMaterial : public Host::Material
+        {
+        private:
+            Device::SimpleMaterial* cu_deviceData;
+
+        public:
+            __host__ SimpleMaterial();
+            __host__ virtual ~SimpleMaterial() { OnDestroyAsset(); }
+
+            __host__ virtual void                       OnDestroyAsset() override final;
+            __host__ virtual void                       OnJson(const Json::Node& jsonNode) override final;
+            __host__ virtual Device::SimpleMaterial*    GetDeviceInstance() const override final { return cu_deviceData; }
+        };
+    }
+}
