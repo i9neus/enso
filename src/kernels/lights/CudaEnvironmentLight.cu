60e06c008733097f1bab89a1bd328eaffecc8edc
Ben Spencer <git@raytracing.co.uk>
2021-06-22 15:51:28 +0100

    Add placeholder for Cuda::EnvironmentLight

---
+++ src/kernels/lights/CudaEnvironmentLight.cu
@@ -0,0 +1,31 @@
+﻿#include "CudaEnvironmentLight.cuh"
+
+namespace Cuda
+{
+    __device__ Device::EnvironmentLight::EnvironmentLight(const BidirectionalTransform& transform) : Light(transform)
+    {
+       
+    }
+
+    __device__ bool Device::EnvironmentLight::Sample(const Ray& incident, const HitCtx& hitCtx, RenderCtx& renderCtx, RayBasic& extant, vec3& L, float& pdfLight) const
+    {
+       
+    }
+
+    __device__ void Device::EnvironmentLight::Evaluate()
+    {
+    }
+
+    __host__  Host::EnvironmentLight::EnvironmentLight()
+        : cu_deviceData(nullptr)
+    {
+        m_hostData.m_transform.MakeIdentity();
+
+        cu_deviceData = InstantiateOnDevice<Device::EnvironmentLight>(m_hostData.m_transform);
+    }
+
+    __host__ void Host::EnvironmentLight::OnDestroyAsset()
+    {
+        DestroyOnDevice(&cu_deviceData);
+    }
+}
---
+++ src/kernels/lights/CudaEnvironmentLight.cuh
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "CudaLight.cuh"
+
+namespace Cuda
+{
+    namespace Host { class EnvironmentLight; }
+
+    namespace Device
+    {
+        class EnvironmentLight : public Device::Light
+        {
+            friend Host::EnvironmentLight;
+        protected:
+            EnvironmentLight() = default;
+
+            float m_emitterArea;
+            vec3  m_emitterRadiance;
+
+        public:
+            __device__ EnvironmentLight(const BidirectionalTransform& transform);
+            __device__ ~EnvironmentLight() = default;
+
+            __device__ bool Sample(const Ray& incident, const HitCtx& hitCtx, RenderCtx& renderCtx, RayBasic& extant, vec3& L, float& pdf) const;
+            __device__ void Evaluate();
+        };
+    }
+
+    namespace Host
+    {
+        class EnvironmentLight : public Host::Light
+        {
+        private:
+            Device::EnvironmentLight* cu_deviceData;
+            Device::EnvironmentLight  m_hostData;
+
+        public:
+            __host__ EnvironmentLight();
+            __host__ virtual ~EnvironmentLight() { OnDestroyAsset(); }
+            __host__ virtual void OnDestroyAsset() override final;
+
+            __host__ virtual Device::EnvironmentLight* GetDeviceInstance() const override final { return cu_deviceData; }
+        };
+    }
+}
