0fba48a9bc413130aa059b5699e46a4142950d9d
Ben Spencer <git@raytracing.co.uk>
2021-08-17 15:36:03 +0100

    Fix broken function call in tests.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -54,7 +54,7 @@ namespace tests
 			{
 				RayBasic ray(vec3(0.0f, 0.0f, 0.0f), normalize(vec3(0.7561f, 1.8375f, -0.276745f)));
 
-				const float t = Intersector::RayUnitBox(ray);
+				const float t = Intersector::RayBox(ray, 0.5f);
 
 				Assert::IsTrue(t == 0.0f, Widen(tfm::format("Ray origin inside box: intersector is %f, should be be 0.0", t)).c_str());
 			}
@@ -63,7 +63,7 @@ namespace tests
 			{
 				RayBasic ray(vec3(-1.0f, -1.0f, -1.0f), normalize(vec3(1.0f, 1.0f, 1.0f)));
 
-				const float t = Intersector::RayUnitBox(ray);
+				const float t = Intersector::RayBox(ray, 0.5f);
 				const float tReference = std::sqrt(3.0f * 0.5f * 0.5f);
 				constexpr float kEpsilon = 1e-6f;
 
@@ -74,7 +74,7 @@ namespace tests
 			{
 				RayBasic ray(vec3(-1.0f, 2.0f, -1.0f), normalize(vec3(1.0f, 1.0f, 1.0f)));
 
-				const float t = Intersector::RayUnitBox(ray);
+				const float t = Intersector::RayBox(ray, 0.5f);
 
 				Assert::IsTrue(t == kNoIntersect, Widen(tfm::format("Ray didn't hit box: intersector is %f, should be be 0.0", t)).c_str());
 			}
