9f836a1dcf6ed7bdc56ced455b51c8084fdc6bf8
Ben Spencer <git@raytracing.co.uk>
2021-09-07 16:10:00 +0100

    Consolidate some code into a shared library so multiple filters can use it

---
+++ src/kernels/lightprobes/CudaLightProbeFilter.cu
@@ -0,0 +1,120 @@
+﻿#include "CudaLightProbeFilter.cuh"
+
+#include "generic/JsonUtils.h"
+
+namespace Cuda
+{
+    __host__ __device__ LightProbeFilterNLMParams::LightProbeFilterNLMParams() : 
+        alpha(1.0f),
+        K(1.0f),
+        patchRadius(1)
+    {
+    }
+
+    __host__ LightProbeFilterNLMParams::LightProbeFilterNLMParams(const ::Json::Node& node)
+    {
+
+    }
+
+    __host__ void LightProbeFilterNLMParams::ToJson(::Json::Node& node) const
+    {
+        node.AddValue("alpha", alpha);
+        node.AddValue("k", K);
+        node.AddValue("patchRadius", patchRadius);
+    }
+
+    __host__ void LightProbeFilterNLMParams::FromJson(const ::Json::Node& node, const uint flags)
+    {
+        node.GetValue("alpha", alpha, flags);
+        node.GetValue("k", K, flags);
+        node.GetValue("patchRadius", patchRadius, flags);
+    }
+
+    __device__ float ComputeNLMWeight(LightProbeFilterGridData& gridData, const LightProbeFilterNLMParams& nlmParams, const ivec3& pos0, const ivec3& posK)
+    {
+        int numValidWeights = 0;
+        float weights[kMaxCoefficients];
+        memset(weights, 0, sizeof(float) * kMaxCoefficients);
+
+        if (!gridData.cu_inputHalfGrid) { return 0.0f; }
+
+        // Iterate over the local block surrounding each element and compute the relative distance
+        for (int w = -nlmParams.patchRadius; w <= nlmParams.patchRadius; ++w)
+        {
+            for (int v = -nlmParams.patchRadius; v <= nlmParams.patchRadius; ++v)
+            {
+                for (int u = -nlmParams.patchRadius; u <= nlmParams.patchRadius; ++u)
+                {
+                    const int probeIdxM = gridData.cu_inputGrid->IdxAt(pos0 + ivec3(u, v, w));
+                    const int probeIdxN = gridData.cu_inputGrid->IdxAt(pos0 + posK + ivec3(u, v, w));
+                    if (probeIdxM < 0 || probeIdxN < 0) { continue; }
+
+                    const vec3* probeM = gridData.cu_inputGrid->At(probeIdxM);
+                    const vec3* probeN = gridData.cu_inputGrid->At(probeIdxN);
+
+                    if (probeM[gridData.coefficientsPerProbe - 1].x < 0.5f || probeN[gridData.coefficientsPerProbe - 1].x < 0.5f) { continue; }
+
+                    const vec3* probeHalfM = gridData.cu_inputHalfGrid->At(probeIdxM);
+                    const vec3* probeHalfN = gridData.cu_inputHalfGrid->At(probeIdxN);
+
+                    for (int coeffIdx = 0; coeffIdx < gridData.coefficientsPerProbe - 1; ++coeffIdx)
+                    {
+                        const vec3& M = probeM[coeffIdx];
+                        const vec3& N = probeN[coeffIdx];
+                        const vec3& halfM = probeHalfM[coeffIdx];
+                        const vec3& halfN = probeHalfN[coeffIdx];
+                        const vec3 varN = sqr((N - halfN) - halfN) * 2.0f;
+                        const vec3 varM = sqr((M - halfM) - halfM) * 2.0f;
+
+                        const vec3 d2 = (sqr(M - N) - nlmParams.alpha * (varN + varM)) /
+                            (vec3(1e-10f) + sqr(nlmParams.K) * (varN + varM));
+                        weights[coeffIdx] += expf(-max(0.0f, cwiseMax(d2)));
+                    }
+                    numValidWeights++;
+                }
+            }
+        }
+
+        // Find the coefficient with the maximum weight and return
+        float maxWeight = kFltMax;
+        for (int coeffIdx = 0; coeffIdx < gridData.coefficientsPerProbe - 1; ++coeffIdx)
+        {
+            maxWeight = min(maxWeight, weights[coeffIdx]);
+        }
+        return maxWeight / float(max(1, numValidWeights));
+    }
+
+    __host__ std::vector<float> GenerateGaussianKernel1D(const int kernelSpan)
+    {
+        // Generates a normalised Gaussian kernel of arbitrary size. The normal distribution is unbounded so we choose a value
+        // of sigma that, when integrated, captures >99% of the area under the curve in the range x = [-1, 1].
+
+        Assert(kernelSpan % 2 == 1);
+
+        std::vector<float> kernel;
+        kernel.resize(kernelSpan);
+
+        const int kernelRadius = kernelSpan / 2;
+        constexpr float sigma = 0.35f;
+        constexpr float recipIntegralNorm = 1 / 0.995841f;
+
+        for (int i = 0; i <= kernelRadius; ++i)
+        {
+            // Compute the sub-interval [x0, x1] that corresponds to the bucket of the kernel element
+            const float x0 = (i / float(kernelSpan)) * 2.0 - 1.0f;
+            const float x1 = ((i + 1) / float(kernelSpan)) * 2.0 - 1.0f;
+
+            // Integrate the Gaussian
+            kernel[i] = kernel[kernelSpan - i - 1] = (AntiderivGauss(x1, sigma) - AntiderivGauss(x0, sigma)) * recipIntegralNorm;
+        }
+
+        return kernel;
+    }
+
+    __host__ std::vector<float> GenerateBoxKernel1D(const int kernelSpan)
+    {
+        Assert(kernelSpan % 2 == 1);
+
+        return std::vector<float>(kernelSpan, 1.0f / kernelSpan);
+    }
+}
---
+++ src/kernels/lightprobes/CudaLightProbeFilter.cuh
@@ -0,0 +1,62 @@
+﻿#pragma once
+
+#include "CudaLightProbeGrid.cuh"
+
+namespace Json { class Node; }
+
+namespace Cuda
+{
+#define kMaxCoefficients 5
+
+    struct LightProbeFilterGridData
+    {
+        const Device::LightProbeGrid*   cu_inputGrid = nullptr;
+        const Device::LightProbeGrid*   cu_inputHalfGrid = nullptr;
+        Device::LightProbeGrid*         cu_outputGrid = nullptr;
+
+        ivec3                           density;
+        int                             numProbes;
+        int                             coefficientsPerProbe;
+    };
+
+    struct LightProbeFilterNLMParams
+    {
+        __host__ __device__ LightProbeFilterNLMParams();
+        __host__ LightProbeFilterNLMParams(const ::Json::Node& node);
+
+        __host__ void ToJson(::Json::Node& node) const;
+        __host__ void FromJson(const ::Json::Node& node, const uint flags);
+
+        float                           alpha;
+        float                           K;
+        int                             patchRadius;
+    };
+
+    __host__ __device__ __forceinline__ float ErfApprox(const float x)
+    {
+        // Closed-form approxiation of the error function.
+        // See 'Uniform Approximations for Transcendental Functions', Winitzki 2003, https://doi.org/10.1007/3-540-44839-X_82
+
+        constexpr float a = 8 * (kPi - 3) / (3 * kPi * (4 - kPi));
+        return copysign(1.0f, x) * sqrt(1 - expf(-(x * x) * (4 / kPi + a * x * x) / (1 + a * x * x)));
+    }
+
+    __host__ __device__ __forceinline__ float AntiderivGauss(const float x, const float sigma)
+    {
+        // The antiderivative of the normalised Gaussian
+        return 0.5f * (1.0f + ErfApprox(x / (sigma * kRoot2)));
+    }
+
+    __host__ __device__ __forceinline__ float Integrate1DGaussian(const float t0, const float t1, const float radius)
+    {
+        // Integrates a Gaussian function whose standard deviation is preset such that its integral between [-radius, radius] is almost 1
+        constexpr float sigma = 0.35f;
+        return (AntiderivGauss(t1 / radius, sigma) - AntiderivGauss(t0 / radius, sigma));
+    }
+
+    __host__ std::vector<float> GenerateGaussianKernel1D(const int kernelSpan);
+
+    __host__ std::vector<float> GenerateBoxKernel1D(const int kernelSpan);
+
+    __device__ float ComputeNLMWeight(LightProbeFilterGridData& gridData, const LightProbeFilterNLMParams& nlmParams, const ivec3& pos0, const ivec3& posK);
+}
