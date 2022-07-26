1f192d7798b37fbef3089a9800247a97e2ee004f
Ben Spencer <git@raytracing.co.uk>
2022-07-26 13:56:55 +0100

    Reorganise directory structure. Give IMGUI its own root folder and move D3D to win.

---
+++ src/imgui/ComponentInterface.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../D3DWindowInterface.h"
+#include "../DXSampleHelper.h"
+
+#include "generic/StdIncludes.h"
+#include "generic/D3DIncludes.h"
+#include "generic/JsonUtils.h"
+#include "manager/RenderManager.h"
+
+#include "shelves/IMGUIAbstractShelf.h"
+#include "shelves/IMGUIShelfFactory.h"
+#include "permutor/IMGUIStateManager.h"
+#include "IMGUIMemoryMonitor.h"
+
+#include "generic/HighResolutionTimer.h"
