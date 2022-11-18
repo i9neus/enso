6ae93b5405f39dc7e554160212c341518b3c33dd
Ben Spencer <git@raytracing.co.uk>
2022-11-18 15:54:51 +0000

    Make HighResolutionTime header-only

--- src/generic/HighResolutionTimer.h
+++ src/generic/HighResolutionTimer.h
@@ -7,14 +7,42 @@
 class HighResolutionTimer
 {
 public:
-    HighResolutionTimer();
-    HighResolutionTimer(const char* msg);
-    HighResolutionTimer(std::function<std::string(float)> lambda);
-    ~HighResolutionTimer();
+    HighResolutionTimer() : m_startTime(std::chrono::high_resolution_clock::now()) {}
 
-    inline float Get() const;
-    inline void Reset();
-    inline void Write(const std::string& format) const;
+    HighResolutionTimer(const char* msg) :
+        m_message(msg),
+        m_startTime(std::chrono::high_resolution_clock::now()) { }
+
+    HighResolutionTimer(std::function<std::string(float)> lambda) :
+        m_lambda(lambda),
+        m_startTime(std::chrono::high_resolution_clock::now()) {}
+
+    ~HighResolutionTimer()
+    {
+        if (m_lambda)
+        {
+            Log::Debug("%s\n", m_lambda(Get()).c_str());
+        }
+        else if (!m_message.empty())
+        {
+            Log::Debug("%s\n", tfm::format(m_message.c_str(), Get()).c_str());
+        }
+    }
+
+    inline float Get() const 
+    {
+        return float(std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - m_startTime).count());
+    }
+
+    inline void Reset()
+    {
+        m_startTime = std::chrono::high_resolution_clock::now();
+    }
+
+    inline void Write(const std::string& format) const
+    {
+        Log::Debug(format, Get());
+    }
 
 private:
     const std::string m_message;
